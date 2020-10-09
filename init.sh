33362444e7e75c8430151e9d18513b969f71b0d8
Ubuntu <ubuntu@ip-10-0-1-149.me-south-1.compute.internal>
2020-10-09 19:01:04 +0000

    init

--- init.sh
+++ init.sh
@@ -3,5 +3,8 @@
 
 
 git clone https://github.com/rayanah/k8s-sandbox.git
-
 git clone https://github.com/rayanah/level-3-project.git
+
+#install tekton
+#kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+#kubectl get pods --namespace tekton-pipelines
