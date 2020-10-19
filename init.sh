b620785fd3346a70893e2148cfe79f7808c68cd9
Ubuntu <ubuntu@ip-10-0-1-210.me-south-1.compute.internal>
2020-10-19 23:01:10 +0000

    for now

--- init.sh
+++ init.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
 
+git clone https://github.com/rayanah/level-3-project.git && cd k8s_sandbox && make up && make install-cicd
 
-git clone https://github.com/rayanah/k8s-sandbox.git
-#make up < to make the cluster and repos and namespaces and instolations
-git clone https://github.com/rayanah/level-3-project.git
-
-#install tekton
-#kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
-#kubectl get pods --namespace tekton-pipelines
